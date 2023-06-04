362f5b654be40272becbee1cbcace1da41e39be6
BluSkreen <jasonduran303@gmail.com>
2023-06-04 13:13:55 -0600

    react-three-fiber testing useFrame hook and organizing files. Now working on pong physics and capturing frames.

--- client/app/pong/page.tsx
+++ client/app/pong/page.tsx
@@ -1,11 +1,19 @@
+'use client';
 
+import * as THREE from 'three'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 
-import { Threejs } from './websockets';
+import { Threejs } from './game';
 
 export default function Pong() {
     return (
+      <>
         <div className='w-[640px] h-[480px] '>
+          <Canvas>
             <Threejs/>
+          </Canvas>
         </div>
-    )
+      </>
+  )
 }
