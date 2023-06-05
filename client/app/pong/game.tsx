4f03fdab6aa4f035bdfbecff5f298fe9f3ced540
BluSkreen <jasonduran303@gmail.com>
2023-06-05 14:56:03 -0600

    moved positioning logic to the main loop in the Threejs component

--- client/app/pong/game.tsx
+++ client/app/pong/game.tsx
@@ -9,35 +9,6 @@ function CPU(props: ThreeElements['mesh']) {
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
 
-    const gls = useThree((state) => state.gl);
-    const gl = gls.domElement.getContext("webgl2");
-    const pixels = new Uint8Array(
-        gl.drawingBufferWidth * gl.drawingBufferHeight * 4
-    );
-    let tick = 0;
-
-    console.log(gl);
-    console.log([gl.drawingBufferWidth, gl.drawingBufferHeight]);
-
-    useFrame(({ clock }) => {
-        const a = clock.getElapsedTime();
-
-        if (tick !== Math.floor(a * 10 % 10)) {
-            tick = Math.floor(a * 10 % 10);
-            gl.readPixels(
-                0,
-                0,
-                gl.drawingBufferWidth,
-                gl.drawingBufferHeight,
-                gl.RGBA,
-                gl.UNSIGNED_BYTE,
-                pixels
-            );
-            //console.log(pixels); // Uint8Array
-            //console.log(a);
-        }
-    });
-
     return (
         <mesh
             {...props}
@@ -57,12 +28,6 @@ function Player(props: ThreeElements['mesh']) {
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
 
-
-    const viewport = useThree((state) => state.viewport)
-    useFrame((state) => {
-        mesh.current.position.setY((state.mouse.y * viewport.height) / 2);
-        mesh.current.updateMatrixWorld();
-    })
 
     return (
         <mesh
@@ -102,26 +67,35 @@ export function Threejs() {
     const [ballPos, setBall] = useState(new THREE.Vector3(0, 0, 0));
     const [cpuPos, setCpu] = useState(new THREE.Vector3(-5, 0, 0));
     const [playerPos, setPlayer] = useState(new THREE.Vector3(5, 0, 0));
-
+    const viewport = useThree((state) => state.viewport)
     let tick = 0;
+    const gls = useThree((state) => state.gl);
+    const gl = gls.domElement.getContext("webgl2");
+    const pixels = new Uint8Array(
+        gl.drawingBufferWidth * gl.drawingBufferHeight * 4
+    );
 
-    useFrame(({ clock }) => {
+    useFrame(({ clock, mouse }) => {
         const a = clock.getElapsedTime();
 
         // check collision
+        // player paddle
+        if (ballPos.x >= playerPos.x && (ballPos.y < playerPos.y + 1 && ballPos.y > playerPos.y - 1)) {
+            velocity[0] = velocity[0] * -1;
+            velocity[1] = velocity[1] + (ballPos.y - playerPos.y) * 0.006
+        }
+        // cpu paddle
+        if (cpuPos.x >= ballPos.x && (ballPos.y < cpuPos.y + 1 && ballPos.y > cpuPos.y - 1)) {
+            velocity[0] = velocity[0] * -1;
+            velocity[1] = velocity[1] + (ballPos.y - cpuPos.y) * 0.006
+        }
+        // walls
+        if (cpuPos.x) {
+        }
 
-        //if (ballPos.x <= playerPos.x && (ballPos.y < playerPos.y + 1 || ballPos.y > playerPos.y - 1)) {
-          //  velocity[0] = velocity[0] * -1;
-        //}
-
-
-
-
-        // moveball
-
+        // movement
         //console.log(`x velocity: 2  t1: ${tick}  t2: ${a}   movement: ${(Math.trunc(tick * 1000) / 1000 - Math.trunc(a * 1000) / 1000) * 2}`);
         //console.log(ballPos[0] - ((Math.trunc(tick * 1000) / 1000 - Math.trunc(a * 1000) / 1000) * velocity[0]));
-
         setBall(
             new THREE.Vector3(
                 ballPos.x - ((Math.trunc(tick * 1000) / 1000 - Math.trunc(a * 1000) / 1000) * velocity[0]),
@@ -129,11 +103,34 @@ export function Threejs() {
                 0
             )
         );
-        // setBall(ballPos[1] - ((Math.trunc(tick * 1000) / 1000 - Math.trunc(a * 1000) / 1000) * velocity[1]));
-        // console.log(a);
-        
-        //
+        setPlayer(
+            new THREE.Vector3(
+                playerPos.x,
+                (mouse.y * viewport.height) / 2,
+                playerPos.z
+            )
+        );
 
+        // caputure pixel data 
+        // every 10th of a second(can change later)
+        if (tick !== Math.floor(a * 10 % 10)) {
+            tick = Math.floor(a * 10 % 10);
+            gl.readPixels(
+                0,
+                0,
+                gl.drawingBufferWidth,
+                gl.drawingBufferHeight,
+                gl.RGBA,
+                gl.UNSIGNED_BYTE,
+                pixels
+            );
+            console.log(pixels); // Uint8Array
+            //console.log(a);
+        }
+        // console.log(gl);
+        // console.log([gl.drawingBufferWidth, gl.drawingBufferHeight]);
+
+        // console.log(a);
         tick = a;
     });
     return (
